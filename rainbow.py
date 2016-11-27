29610166b777e62d959d1fdfa0f081bd38728e6f
nxb303 <nxb303@gmail.com>
2016-11-27 19:51:12 +0100

    rainbow

---
+++ rainbow.py
@@ -0,0 +1,36 @@
+import socket
+from time import sleep
+import random
+
+UDP_IP = "192.168.3.102"
+UDP_PORT = 2390
+sleepytime = 0.02
+maxbrightness = 255
+
+while True:
+    for x in range(0, maxbrightness):
+        y = maxbrightness - x
+        packet = "%d:%d:0" % (x, y)
+        sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+        sock.sendto(packet, (UDP_IP, UDP_PORT))
+        sock.close()
+        sleep(sleepytime)
+
+    for x in range(0, maxbrightness):
+        y = maxbrightness - x
+        packet = "%d:0:%d" % (y, x)
+        sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+        sock.sendto(packet, (UDP_IP, UDP_PORT))
+        sock.close()
+        sleep(sleepytime)
+
+    for x in range(0, maxbrightness):
+        y = maxbrightness - x
+        packet = "0:%d:%d" % (x, y)
+        sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+        sock.sendto(packet, (UDP_IP, UDP_PORT))
+        sock.close()
+        sleep(sleepytime)
